effe5989e301a229e0c6e226de61c413614bbfc6
dani <>
2022-02-24 06:30:14 +0100

    switch to dev

---
+++ src/Pages/EventView/EventView.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const EventView = props => {
+  return (
+      <div>
+          
+    </div>
+  )
+}
+
+EventView.propTypes = {}
+
+export default EventView
