df3ebe79264e5b1982b6edd0f93f8a931c2749ab
maildedani99 <maildedani9@gmail.com>
2022-05-20 10:19:59 +0200

    login modal restyling

--- src/Conponents/RegisterModal/RegisterModal.js
+++ src/Conponents/RegisterModal/RegisterModal.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
 import logo from "../../assets/iwblue.png";
-import google from "../../assets/google.png";
-
 import {
   focusInputStyles,
   focusSearchStyles,
@@ -14,29 +12,38 @@ import {
 import Button from "../Button/index";
 import InputText from "../InputText/InputText";
 
-const LoginModal = ({ loginModalOpened, setLoginModalOpened }) => {
-  const onCloseModal = () => setLoginModalOpened(false);
+const RegisterModal = ({ registerModalOpened, setRegisterModalOpened }) => {
+
+    const onCloseModal = () => setRegisterModalOpened (false);
+
 
   return (
     <div>
-      <Modal open={loginModalOpened} onClose={onCloseModal} center>
+      <Modal open={registerModalOpened} onClose={onCloseModal} center>
         <div className="text-5xl mt-8 flex flex-col items-center	font-bold	  text-center	">
           <img src={logo} alt="iwas" width="50px" />
           <span className="mt-8">Bienvenido a Iwas</span>
           <InputText
-            className="mt-20 text-xl"
+            className="mt-20 text-2xl"
             style={inputStyles}
             focusStyle={focusInputStyles}
             placeholder="Correo"
           />
           <InputText
-            className="mt-5 text-xl"
+            className="mt-5 text-2xl"
             style={inputStyles}
             focusStyle={focusInputStyles}
             placeholder="Contraseña"
           />
-          <span className="text-sm mt-4 mb-8">¿Olvidaste tu contraseña?</span>
-          <div className="text-2xl w-1/2 mb-6 ">
+          <InputText
+            className="mt-5 text-2xl"
+            style={inputStyles}
+            focusStyle={focusInputStyles}
+            placeholder="Verifica contraseña"
+          />
+
+          <span className="text-sm mt-4 mb-16">¿Olvidaste tu contraseña?</span>
+          <div className="text-4xl w-1/2 mb-16 ">
             <Button
               value="Iniciar sesión"
               onClick={() => console.log("inciar sesion click")}
@@ -44,11 +51,6 @@ const LoginModal = ({ loginModalOpened, setLoginModalOpened }) => {
               //backgroundColor="#e60023"
               backgroundColor="#04a5a4"
             />
-          </div>
-          <div className="text-xl font-normal mt-4 mb-6">o</div>
-          <div className="flex flex-row text-2xl border rounded-full p-3 mb-16">
-            <img src={google} alt="google" width="48px" />
-            <Button value="Continuar con Google" />
           </div>
           <div className="flex w-6/12 text-sm font-normal 	">
             <p>
@@ -63,6 +65,6 @@ const LoginModal = ({ loginModalOpened, setLoginModalOpened }) => {
   );
 };
 
-LoginModal.propTypes = {};
+RegisterModal.propTypes = {};
 
-export default LoginModal;
+export default RegisterModal;
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -4,6 +4,19 @@ export const inputStyles = {
   borderStyle: "solid",
   borderWidth: "2px",
   padding: "10px",
+  width: "50%",
+  alignSelf: "center",
+  outline: "none",
+  
+};
+export const focusInputStyles = {
+  appearance: "none",
+  borderRadius: "16px",
+  borderStyle: "solid",
+  padding: "10px",
+  width: "50%",
+  alignSelf: "center",
+  outline: "2px solid #04a5a4",
 };
 
 export const searchStyles = {
@@ -18,12 +31,11 @@ export const searchStyles = {
 };
 export const focusSearchStyles = {
   appearance: "none",
-  border: "2px solid #04a5a4",
   borderRadius: "16px",
   borderStyle: "solid",
   padding: "10px",
   width: "50%",
   alignSelf: "center",
-  outline: "red",
+  outline: "2px solid #04a5a4",
 };
 
